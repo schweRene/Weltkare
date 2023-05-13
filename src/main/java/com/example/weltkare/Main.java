f8c25ac649c5771b935b553120f8084a3899704e
schweRene <106735291+schweRene@users.noreply.github.com>
2023-05-13 21:53:36 +0200

    Initial commit

---
+++ src/main/java/com/example/weltkare/Main.java
@@ -0,0 +1,24 @@
+package com.example.weltkare;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = new FXMLLoader.load(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
